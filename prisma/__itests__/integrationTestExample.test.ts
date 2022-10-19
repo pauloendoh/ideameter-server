0de198ef03677e0520ccd51c80d77d04887e936f
Paulo Endoh <paulo.endoh@gmail.com>
2022-10-19 20:40:24 -0300

    test/cl835ylfh21790m3dcqezkiov

--- prisma/__itests__/integrationTestExample.test.ts
+++ prisma/__itests__/integrationTestExample.test.ts
@@ -1,49 +1,77 @@
-import { describe, expect, it } from "vitest"
+import { beforeEach, describe, expect, it } from "vitest"
 import AuthRepository from "../../src/domains/auth/AuthRepository"
 import TabRepository from "../../src/domains/group/group-tab/TabRepository"
 import GroupRepository from "../../src/domains/group/GroupRepository"
 import IdeaRepository from "../../src/domains/idea/IdeaRepository"
 import { buildIdeaWithRelations } from "../../src/types/domain/idea/IdeaWithRelationsType"
 
-describe("integration test example", async () => {
-  describe("when an idea has high impact votes is deleted", () => {
-    it("should not throw error", async () => {
-      const fn = async () => {
-        const authRepo = new AuthRepository()
-        const groupRepo = new GroupRepository()
-        const tabRepo = new TabRepository()
-        const ideaRepo = new IdeaRepository()
+describe("IdeaRepository.i.test.ts", async () => {
+  describe("when user creates a group, tab and idea", async () => {
+    let authRepo: AuthRepository
+    let groupRepo: GroupRepository
+    let tabRepo: TabRepository
+    let ideaRepo: IdeaRepository
 
-        const user = await authRepo.registerNewUser(
-          "test@test.com",
-          "test",
-          "lmao"
-        )
+    let user: Awaited<ReturnType<typeof authRepo.registerNewUser>>
+    let group: Awaited<ReturnType<typeof groupRepo.createGroup>>
+    let tab: Awaited<ReturnType<typeof tabRepo.createTab>>
+    let idea: Awaited<ReturnType<typeof ideaRepo.saveIdea>>
 
-        const group = await groupRepo.createGroup(
-          { id: "", description: "", name: "group" },
-          user.id
-        )
+    beforeEach(async () => {
+      authRepo = new AuthRepository()
+      groupRepo = new GroupRepository()
+      tabRepo = new TabRepository()
+      ideaRepo = new IdeaRepository()
 
-        const tab = await tabRepo.createTab(group.id, "tab", user.id)
+      let username = new Date().toISOString()
+      user = await authRepo.registerNewUser(
+        username + "@test.com",
+        username,
+        "lmao"
+      )
 
-        const idea = await ideaRepo.saveIdea(
-          buildIdeaWithRelations({ tabId: tab.id }),
-          user.id
-        )
+      group = await groupRepo.createGroup(
+        { id: "", description: "", name: "group" },
+        user.id
+      )
 
-        const ideaWithVote = await ideaRepo.saveIdea(
-          {
-            ...idea,
-            highImpactVotes: [{ ideaId: idea.id, userId: user.id }],
-          },
-          user.id
-        )
+      tab = await tabRepo.createTab(group.id, "tab", user.id)
 
-        await ideaRepo.deleteIdea(ideaWithVote.id)
-      }
+      idea = await ideaRepo.saveIdea(
+        buildIdeaWithRelations({ tabId: tab.id }),
+        user.id
+      )
+    })
 
-      await expect(fn()).resolves.toBe(true)
+    describe("and when user deletes tab", () => {
+      it("should not throw error", async () => {
+        const sut = async () => {
+          await tabRepo.deleteGroupTab(tab.id)
+          return true
+        }
+
+        await expect(sut()).resolves.toBe(true)
+      })
+    })
+
+    describe("and when an idea has high impact votes is deleted", () => {
+      it("should not throw error", async () => {
+        const asyncFn = async () => {
+          const ideaWithVote = await ideaRepo.saveIdea(
+            {
+              ...idea,
+              highImpactVotes: [{ ideaId: idea.id, userId: user.id }],
+            },
+            user.id
+          )
+
+          await ideaRepo.deleteIdea(ideaWithVote.id)
+
+          return true
+        }
+
+        await expect(asyncFn()).resolves.toBe(true)
+      })
     })
   })
 })
